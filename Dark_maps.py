543299d0985ae5f03b7668c582b588da2787c187
BadRedCrab <37663893+BadRedCrab@users.noreply.github.com>
2023-11-11 21:29:12 +0300

    Add files via upload

---
+++ Dark_maps.py
@@ -0,0 +1,146 @@
+maps = [[[{"MapName": "up,down,left,right", "id": 4, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 48], ['box', 0, 44, 48, 48], ['box', 75, 0, 79, 48], ['box', 75, 44, 127, 48], ['box', 75, 75, 127, 79], ['box', 75, 75, 79, 127], ['box', 0, 75, 48, 79], ['box', 44, 75, 48, 127]],
+[[{"MapName": "up,down,left", "id": 31, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 48], ['box', 0, 44, 48, 48], ['box', 75, 0, 79, 127], ['box', 0, 75, 48, 79], ['box', 44, 75, 48, 127]],
+[[{"MapName": "up,down,right", "id": 32, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 127], ['box', 75, 0, 79, 48], ['box', 75, 44, 127, 48], ['box', 75, 75, 127, 79], ['box', 75, 75, 79, 127]],
+[[{"MapName": "up,left,right", "id": 33, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 48], ['box', 0, 44, 48, 48], ['box', 75, 0, 79, 48], ['box', 75, 44, 127, 48], ['box', 0, 75, 127, 79]],
+[[{"MapName": "down,left,right", "id": 34, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 0, 44, 127, 48], ['box', 75, 75, 127, 79], ['box', 75, 75, 79, 127], ['box', 0, 75, 48, 79], ['box', 44, 75, 48, 127]],
+[[{"MapName": "up,down", "id": 251, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 127], ['box', 75, 0, 79, 127]],
+[[{"MapName": "left,right", "id": 252, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 0, 44, 127, 48], ['box', 0, 75, 127, 79]],
+[[{"MapName": "up,left", "id": 21, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 48], ['box', 0, 44, 48, 48], ['box', 75, 0, 79, 79], ['box', 0, 75, 79, 79]],
+[[{"MapName": "down,left", "id": 22, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 0, 44, 79, 48], ['box', 75, 44, 79, 127], ['box', 0, 75, 48, 79], ['box', 44, 75, 48, 127]],
+[[{"MapName": "down,right", "id": 23, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 48, 44, 127, 48], ['box', 75, 75, 79, 127], ['box', 79, 75, 127, 79], ['box', 44, 44, 48, 127]],
+[[{"MapName": "up,right", "id": 24, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 79], ['box', 75, 0, 79, 48], ['box', 75, 44, 127, 48], ['box', 44, 75, 127, 79]],
+[[{"MapName": "left", "id": 11, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 0, 44, 79, 48], ['box', 75, 44, 79, 79], ['box', 0, 75, 79, 79]],
+[[{"MapName": "right", "id": 12, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 44, 127, 48], ['box', 44, 75, 127, 79], ['box', 44, 44, 48, 75]],
+[[{"MapName": "down", "id": 13, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 48, 44, 79, 48], ['box', 75, 44, 79, 127], ['box', 44, 44, 48, 127]],
+[[{"MapName": "up", "id": 14, "type": 1 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}], ['box', 44, 0, 48, 79], ['box', 75, 0, 79, 79], ['box', 44, 75, 79, 79]],
+[[{"MapName": "up", "id": -1, "type": 0 }], ["player", {"start":[61,61,10], "up":[61,5,10], "down":[61,122,10], "left":[5,61,10], "right":[122,61,10]}]]
+]
+
+def map_generator3(seed=-1, DEBAG = 0,STR = 5): #генератор карты v3
+    import random
+    try:
+        seed=int(seed) #инициализация сида
+    except:
+        print(f"Error: -1 < SEED < 2^32\nYou: {seed}")
+        seed=-1
+    if seed==-1:
+        seed=random.randint(0, 2**32)
+    random.seed(seed)
+    print("seed =",seed)
+    import numpy as np
+    original = np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0], #пресет карты с начальной комнатой
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+    np.random.seed(seed)
+    noise = np.random.uniform(0, .1, (9,9)) #карта шума для генерации карты
+    noise1 = [] #временные переменные
+    noise2 = []
+    for i in range(9): #перевод шума в обычный массив (устранение бага при генерации с автоупрощеннием)
+        for j in range(9):
+            noise1.append(noise[i][j])
+        noise1.append(-2)
+        noise2.append(noise1)
+        noise1 = []
+    noise2.append([-2,-2,-2,-2,-2,-2,-2,-2,-2,-2]) #добавление границы для генератора
+    noise = noise2
+
+    def line(original, noise, x1, y1,debag=1): #генератор вектора комнат
+        x2,y2 = x1,y1
+        if debag > 0:
+            print()
+        if debag>1:
+            print(noise)
+        def find(): #поиск максимального веса в области 3х3
+            A = []
+            B = []
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    if i==0 or j==0:
+                        try:
+                            A.append(noise[x1+i][y1+j])
+                            B.append((i,j))
+                        except:
+                            pass
+            if debag > 0:
+                print(A,"\n","№",A.index(max(A)),"| x1=",x1,"| y1=",y1,B[A.index(max(A))])
+            return B[A.index(max(A))][0],B[A.index(max(A))][1]
+
+        for i in range(5):
+            noise[x1][y1] = -1
+            x2,y2 = find()
+            x1+=x2
+            y1+=y2
+            original[x1][y1] = 1
+            noise[x1][y1] = -1
+            if debag > 0:
+                print(original)
+            if debag > 1:
+                print(noise)
+
+        A=[] #
+        for i in range(9):
+            for j in range(9):
+                if noise[i][j] == -1:
+                    A.append((j, i))
+        from random import choice
+        if debag > 0:
+            print("кол-во комнат:", len(A))
+        A = choice(A)
+        if debag > 0:
+            print("выбор следуещего узла:", A)
+        return A[1],A[0]
+
+    x,y = 4,4
+    for i in range(STR):
+        x,y =line(original, noise, x, y, debag=DEBAG)
+
+    """""""""
+    noise1.clear()
+    noise2.clear()
+    for i in range(9):  # перевод карты в обычный массив
+        for j in range(9):
+            noise1.append(original[i][j])
+        noise2.append(noise1)
+        noise1 = []
+    original = noise2
+    """""
+    print("MAP:")
+    #print(original)
+
+    def init_room(map): #модуль для верной генерации карты комнат
+        id_map= np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0]])
+        patern = [(0, -1), (-1, 0), (1, 0), (0, 1)]
+        type_list = {(0, 0, 0, 0):-1, (1, 0, 0, 0):14, (0, 1, 0, 0):11, (0, 0, 1, 0):12, (0, 0, 0, 1):13, (1, 1, 0, 0):21, (0, 1, 0, 1):22, (0, 0, 1, 1):23, (1, 0, 1, 0):24, (1, 0, 0, 1):251, (0, 1, 1, 0):252, (0, 1, 1, 1):34, (1, 1, 1, 0):33, (1, 0, 1, 1):32, (1, 1, 0, 1):31, (1, 1, 1, 1):4}
+        for y in range(9):
+            for x in range(9):
+                if map[y][x]==1:
+                    type = [0, 0, 0, 0]
+                    try:
+                        for i in range(4):
+                            if map[y + patern[i][1]][x + patern[i][0]]==1:
+                                type[i]=1
+                    except:
+                        pass
+                    id_map[y][x] = type_list[(type[0],type[1],type[2],type[3])]
+        return id_map
+
+
+    id_map = init_room(original)
+    print(id_map)
+    return original, id_map
+
+#print(map_generator3(-1)[1])
